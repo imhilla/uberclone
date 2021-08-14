f6351f6340c3d303ca1fcbb82e0d413e284f5a5d
imhilla <hillaryodhiambo@gmail.com>
2021-08-14 11:57:36 +0300

    add navslice

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
-// set up redux
-
+import { store } from './store'
 export default function App() {
 
   return (
-    <Provider>
+    <Provider store={store}>
       <View style={styles.container}>
         <Text>Let's build UBER CLONE</Text>
       </View>
